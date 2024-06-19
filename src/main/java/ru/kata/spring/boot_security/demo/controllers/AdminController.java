787b687a271953f095d06bbc85ca57ac08519762
Pyotr Gribanov <Tridxy@gmail.com>
2024-06-19 13:09:34 +0300

    fixed 19.06.2024

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,20 +1,20 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UserService;
+import ru.kata.spring.boot_security.demo.util.*;
 
-import javax.annotation.PreDestroy;
+import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
 
-@Controller
-@RequestMapping("/admin")
+@RestController
+@RequestMapping("/api/admin")
 public class AdminController {
 
     private final UserService userService;
@@ -27,32 +27,47 @@ public class AdminController {
     }
 
     @GetMapping()
-    public String getAdminPanel(Model model, Principal principal) {
-        model.addAttribute("allUsers", userService.getAllUsers());
-        model.addAttribute("authUser", userService.getUserByUserName(principal.getName()));
-        model.addAttribute("newUser", new User());
-        model.addAttribute("roles", roleService.getRoles());
-        model.addAttribute("activeTable", "usersTable");
-        return "admin/adminPanel";
+    public ResponseEntity<User> getAdminInfo(Principal principal) {
+        System.out.println("--------------------");
+        System.out.println(principal.getName());
+        System.out.println("--------------------");
+        return new ResponseEntity<>(userService.getUserByUserName(principal.getName()), HttpStatus.OK);
     }
 
-    @PostMapping("/update")
-    public String updateUser(@ModelAttribute("updateUser") User updateUser,
-                             @RequestParam(value = "roleStringNameArray", required = false) List<String> roleArray) {
-        userService.updateUser(updateUser, roleArray);
-        return "redirect:/admin";
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
-    @PostMapping("/delete")
-    public String deleteUser(@RequestParam("id") int id) {
-        userService.deleteUserById(id);
-        return "redirect:/admin";
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Integer id) {
+        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
     }
 
     @PostMapping()
-    public String addUser(@ModelAttribute("newUser") User newUser,
-                          @RequestParam(value = "roleStringNameArray", required = false) List<String> roleArray) {
-        userService.saveUser(newUser, roleArray);
-        return "redirect:/admin";
+    public ResponseEntity<HttpStatus> createUser(@RequestBody @Valid User user,
+                                                 @RequestParam(name = "roles", required = false) List<String> roles) {
+        userService.saveUser(user, roles);
+        return ResponseEntity.status(HttpStatus.CREATED).body(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable(name = "id") Integer id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(HttpStatus.OK);
+    }
+
+    @PutMapping()
+    public ResponseEntity<HttpStatus> updateUser(@RequestBody @Valid User updateUser,
+                                                 @RequestParam(name = "roles", required = false) List<String> roles) {
+        userService.updateUser(updateUser, roles);
+        return ResponseEntity.status(HttpStatus.OK).body(HttpStatus.OK);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<PersonErrorResponse> handleException(PersonNotFoundException ex) {
+        PersonErrorResponse response =
+                new PersonErrorResponse("User with id wasn't found!", System.currentTimeMillis());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AppHomeController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AppHomeController.java
@@ -1,7 +1,8 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 public class AppHomeController {
